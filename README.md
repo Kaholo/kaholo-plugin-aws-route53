996b2dd5e02ff829d879a57f4b7c3b5af2790f61
Amit Alon <amit@kaholo.io>
2021-03-25 11:19:38 +0200

    minor fixes and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# kaholo-plugin-aws-route53
+# kaholo-plugin-aws-eks
 AWS Rotue53 plugin for Kaholo
+
+## Settings
+1. Access Key ID(vault)
+2. Secret Access key(vault)
+
+## Method Create Record
+Create a resource record set, which contains authoritative DNS information for a specified domain name or subdomain name. [documentation](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Route53.html#changeResourceRecordSets-property)
+
+### Parameters
+1. Access Key ID(vault)**optional**
+2. Secret Access key(vault)**optional**
+3. Hosted Zone ID(string)**required** - The ID of the hosted zone that you want to create tuehe record in.
+4. Name(string)**required** - The name of the record that you want to create.
+5. Record Type(options)**required** - The DNS record type.
+6. Value(string)**required** - The new DNS record value, not to exceed 4,000 characters.
+7. TTL(int)**optional** - The resource record cache time to live (TTL), in seconds. Default is 60.
+8. Comment(string)**optional** - Any comments you want to include about the Create request.
+
+## Change Resource Record Sets
+Creates, changes, or deletes resource record sets. [documentation](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Route53.html#changeResourceRecordSets-property)
+
+### Parameters
+1. Access Key ID(vault)**optional**
+2. Secret Access key(vault)**optional**
+3. Hosted Zone ID(string)**required** - The ID of the hosted zone that you want to change record sets in it.
+4. Changes Array(array)**required** - An array of objects, each representing a change to do in the record set. You can see the possible and required fields for this object in the [documentation](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Route53.html#changeResourceRecordSets-property) in the changes field inside the json request.
+5. Comment(string)**optional** - Any comments you want to include about this batch request.
--- src/app.js
+++ src/app.js
@@ -1,65 +1,40 @@
 const aws = require("aws-sdk");
 
-function getRoute53(action, settings) {
-  return new aws.Route53({
-    accessKeyId: action.params.AWS_ACCESS_KEY_ID || settings.AWS_ACCESS_KEY_ID,
-    secretAccessKey: action.params.AWS_SECRET_ACCESS_KEY || settings.AWS_SECRET_ACCESS_KEY,
-  });
-}
-
 function createRecord(action, settings) {
-  return new Promise((resolve, reject) => {
-    if (
-      !action.params.name ||
-      !action.params.value ||
-      !action.params.type ||
-      !action.params.hostedZoneId
-    )
-      return reject("One or more fields are missing");
+  if (!action.params.name || !action.params.value ||
+      !action.params.type || !action.params.hostedZoneId){
+    throw "One or more fields are missing";
+  }
 
-    let params = {
-      ChangeBatch: {
-        Changes: [
-          {
-            Action: "CREATE",
-            ResourceRecordSet: {
-              Name: action.params.name,
-              ResourceRecords: [
-                {
-                  Value: action.params.value,
-                },
-              ],
-              TTL: action.params.ttl || 60,
-              Type: action.params.type,
-            },
-          },
-        ],
-        Comment: action.params.comment || "",
-      },
-      HostedZoneId: action.params.hostedZoneId,
-    };
-
-    let route53 = getRoute53(action, settings);
-    route53.changeResourceRecordSets(params, function (err, data) {
-      if (err) {
-        return reject("Could not create record: " + err);
-      }
-      return resolve(data);
-    });
-  });
+  const changes = [{
+    Action: "CREATE",
+    ResourceRecordSet: {
+      Name: action.params.name,
+      ResourceRecords: [
+        {
+          Value: action.params.value,
+        },
+      ],
+      TTL: action.params.ttl || 60,
+      Type: action.params.type,
+    },
+  }];
+  return changeRecordSetsSDK(action, settings, changes);
 }
 
 function changeResourceRecordSets(action, settings) {
-  return new Promise((resolve, reject) => {
-    if(!action.params.hostedZoneId || !action.params.changesArray){
-        return reject("Hosted zond ID and changes array required!");
-    }
-    if (!Array.isArray(action.params.changesArray)){
-        return reject("Changes must be array");
-    }
+  if(!action.params.hostedZoneId || !action.params.changesArray){
+    throw "Hosted zond ID and changes array required!";
+  }
+  if (!Array.isArray(action.params.changesArray)){
+    throw "Changes must be array";
+  }
+  return changeRecordSetsSDK(action, settings, action.params.changesArray);
+}
 
-    const route53 = getRoute53(action, settings);
+// helpers
 
+async function changeRecordSetsSDK(action, settings, changes){
     const params = {
       ChangeBatch: {
         Changes: action.params.changesArray,
@@ -67,11 +42,19 @@ function changeResourceRecordSets(action, settings) {
       },
       HostedZoneId: action.params.hostedZoneId,
     };
-
-    route53.changeResourceRecordSets(params, (err,result)=>{
+    const client = getRoute53(action, settings);
+    return new Promise((resolve, reject) => {
+      client.changeResourceRecordSets(params, (err,result)=>{
         if(err) return reject(err);
         resolve(result);
-    })
+      });
+    });
+}
+
+function getRoute53(action, settings) {
+  return new aws.Route53({
+    accessKeyId: action.params.AWS_ACCESS_KEY_ID || settings.AWS_ACCESS_KEY_ID,
+    secretAccessKey: action.params.AWS_SECRET_ACCESS_KEY || settings.AWS_SECRET_ACCESS_KEY,
   });
 }
 
